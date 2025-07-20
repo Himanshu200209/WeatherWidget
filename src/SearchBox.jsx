3acb96a504f691b87a52e54154f35b2c4befd787
Himanshu Mishra <himanshumishra12092002@gmail.com>
2025-07-20 13:43:04 +0530

    Fix :api issue

--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -10,13 +10,15 @@ export default function SearchBox({updateInfo}){
   let [city,setCity] = useState("");
   let [error,setError] = useState(false);
 
-  let API_URL = "api.openweathermap.org/data/2.5/weather";
+ let API_URL = "https://api.openweathermap.org/data/2.5/weather";
+
   let API_KEY = "759152fb01eb2433f1d535b4aa30428d";
 
   let getWeatherInfo = async()=>{
     try {
 
-      let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`);
+     let response = await fetch(`${API_URL}?q=${city}&appid=${API_KEY}&units=metric`);
+
 
   let jsonResponse= await response.json();
   console.log(jsonResponse);
